adbed0bfb029b6f4578af32c25666b2c3fbf750c
matias <mathewb97@hotmail.com>
2024-05-13 11:17:02 +0200

    minor

--- src/main/Main.java
+++ src/main/Main.java
@@ -13,7 +13,7 @@ import java.io.*;
 import java.math.BigInteger;
 import java.util.*;
 
-public class ModelCountingRanking {
+public class Main {
 
     public static void main(String[] args) throws IOException, InterruptedException {
         String outname = null;
