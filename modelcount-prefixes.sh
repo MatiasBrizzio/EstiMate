f10664a1d6dfbc0ec189edd7ca7184553926cc7d
matias <mathewb97@hotmail.com>
2024-05-14 12:10:38 +0200

    cleaning libs

--- modelcount-prefixes.sh
+++ modelcount-prefixes.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -Xmx8g -Djava.library.path=/usr/local/lib -cp bin/.:lib/* main.CountMain "$@"
+java -Xmx8g -Djava.library.path=/usr/local/lib -cp bin/.:lib/* helpers.CountMain "$@"
